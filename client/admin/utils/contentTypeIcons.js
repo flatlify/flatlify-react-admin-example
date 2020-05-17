adbd8a79ad101a6caccb6da10601642ef46bf0ea
Andrew Yushkevich <and.yushkevich@gmail.com>
2020-05-17 15:37:02 +0300

    Revert "feat: select any icon in content type"
    This reverts commit 480ab6b00bdc0a0f3e94adee0ec4d0b16b06ab78.

--- client/admin/utils/contentTypeIcons.js
+++ client/admin/utils/contentTypeIcons.js
@@ -1,3 +1,26 @@
-import icons from '@material-ui/icons';
+import BookIcon from '@material-ui/icons/Book';
+import ExplicitIcon from '@material-ui/icons/Explicit';
+import AttachmentIcon from '@material-ui/icons/Attachment';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import PhoneIcon from '@material-ui/icons/Phone';
+import TvIcon from '@material-ui/icons/Tv';
+import AirplayIcon from '@material-ui/icons/Airplay';
 
-export const getIconFromName = name => icons[name];
+export const getIconFromName = name => {
+  switch (name) {
+    case 'AirplayIcon':
+      return AirplayIcon;
+    case 'ExplicitIcon':
+      return ExplicitIcon;
+    case 'AttachmentIcon':
+      return AttachmentIcon;
+    case 'TextFieldsIcon':
+      return TextFieldsIcon;
+    case 'PhoneIcon':
+      return PhoneIcon;
+    case 'TvIcon':
+      return TvIcon;
+    default:
+      return BookIcon;
+  }
+};
